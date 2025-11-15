e7316950b475a82fa0f3bd42423123d409031538
Monish Ramalingam <155383938+monish-io@users.noreply.github.com>
2025-11-15 17:27:34 +0530

    Create uartserial.c

---
+++ uart/uartserial/uartserial.c
@@ -0,0 +1,22 @@
+#include<LPC21xx.h>
+void UART0_TX(unsigned char);
+void UART0_CONFIG(void);
+void UART0_CONFIG(){
+	PINSEL0=0x5;
+	U0LCR=0x83;
+	U0DLL=97;
+	U0DLM=0;
+	U0LCR=0x03;
+}
+void UART0_TX(unsigned char txByte){
+	while(!((U0LSR>>5)&1));
+	U0THR=txByte;
+}
+int main(){
+	unsigned char x='A';
+	UART0_CONFIG();
+	while(1){
+		UART0_TX(x++);
+		if(x=='Z') x='A';
+	}
+}
